01c5506f4acf919aed52e3aab64154dc4b683828
Nguyen-Kim-Tien <tien.nguyenctcxht11@hcmut.edu.vn>
2023-04-10 09:01:49 +0700

    update database

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost','root','111202','shop_db') or die('connection failed');
+$conn = mysqli_connect('localhost','root','Nguyenkimtien1','shop_db') or die('connection failed');
 $image_foldr = 'uploaded_img/';
 $default_img = '201818.png';
 ?>
